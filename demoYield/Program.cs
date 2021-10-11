1a4d3bbf6ae8884f8b31a5eff00b26e84d28f64b
Aurélien <aurelien.strimelle@bstorm.be>
2021-10-11 09:19:52 +0200

    Ajoutez des fichiers projet.

---
+++ demoYield/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace demoYield
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IEnumerable<string> d1 = Demo1();
+
+            foreach(string e in d1)
+            {
+                Console.WriteLine(e);
+            }
+
+            IEnumerable<int> d2 = Generateur();
+
+            foreach (int e in d2)
+            {
+                Console.WriteLine(e);
+            }
+        }
+
+        private static IEnumerable<string> Demo1()
+        {
+            List<string> textes = new List<string>()
+            {
+                "Riri", "Fifi", "Zaza", "Della", "Gontran"
+            };
+
+            foreach(string element in textes) {
+                yield return element.ToUpper();
+            }
+        }
+
+        private static IEnumerable<int> Generateur()
+        {
+            yield return 42;
+            yield return 13;
+            for (int i = 0; i < 10; i++)
+            {
+                yield return i;
+            }
+            yield return 42;
+        }
+    }
+}
